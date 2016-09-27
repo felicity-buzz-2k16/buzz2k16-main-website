69697b1accbfa7dc0a4a22d6000d0e27d39c42f5
Megh Parikh <meghprkh@gmail.com>
2016-09-27 23:35:02 +0530

    Improve styling of dialogues

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ update = function() {
   game.physics.arcade.collide(player, sprites, function (pl, character) {
     if (isModalOpen) return
     isModalOpen = true
-    picoModal(`<img src="./assets/images/${character.properties.name}.jpg"><p>"${character.properties.dialogue}"</p>`)
+    picoModal(`<img src="./assets/images/${character.properties.name}.jpg" width="${window.innerWidth*0.5}"><p align="center">"${character.properties.dialogue}"</p>`)
         .afterClose(modalClosed)
         .show();
     game.paused = true
