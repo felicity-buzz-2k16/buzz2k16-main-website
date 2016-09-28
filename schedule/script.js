11e6329d64e9ed651a7fa47fb867a587a1f260ad
Anubhab Sen <anubhabsen@gmail.com>
2016-09-28 18:25:38 +0530

    Adds fu;; schedule

--- schedule/script.js
+++ schedule/script.js
@@ -1,16 +1,48 @@
 var scheduleJSON = [
   {
-    date: '28 September',
+    date: '29 September',
     events: [
-      {name: 'Cache In', time: '12:00 AM', href: 'cachein'},
-      {name: 'Cache In', time: '12:00 AM', href: 'cachein'},
+      {name: 'Cache In', time: '08:00 AM', href: 'cachein'},
+      {name: 'Hack In', time: '09:00 PM', href: 'hackin'},
     ]
   },
 
   {
-    date: '29 September',
+    date: '30 September',
     events: [
-      {name: 'Cache In', time: '12:00 AM', href: 'cachein'},
+      {name: 'Gordian Knot', time: '08:00 AM', href: 'gordianknot'},
+      {name: 'Sports(Footsal, 21 Points, Gully Cricket)', time: '05:00 PM', href: 'sports'},
+      {name: 'Rubik\'s Cube', time: '06:00 PM', href: 'rubikscube'},
+      {name: 'Rangoli Making', time: '06:00 PM', href: 'kalakshetra'},
+      {name: 'Na Show Na Estam', time: '08:00 PM', href: 'misc'},
+      {name: 'Zombie Zone', time: '07:00 PM', href: 'gaming'},
+      {name: 'Lit Quiz', time: '08:00 PM', href: 'lit'},
+    ]
+  },
+  {
+    date: '1 October',
+    events: [
+      {name: 'Poster Making', time: '03:30 PM', href: 'kalakshetra'},
+      {name: 'Pokemon Battling', time: '03:30 PM', href: 'gaming'},
+      {name: 'Sports(Footsal, 21 Points, Gully Cricket)', time: '05:00 PM', href: 'sports'},
+      {name: 'JAM/ Air Crash', time: '06:00 PM', href: 'jam'},
+      {name: 'Design It', time: '06:00 PM', href: 'designit'},
+      {name: 'CodeCraft', time: '08:30 PM', href: 'codecraft'},
+      {name: 'Zombie Zone', time: '07:00 PM', href: 'gaming'},
+    ]
+  },
+  {
+    date: '2 October',
+    events: [
+      {name: 'Sports(Footsal, 21 Points, Gully Cricket)', time: '08:00 AM', href: 'sports'},
+      {name: 'Arduino', time: '09:00 PM', href: 'arduino'},
+      {name: 'Face Painting', time: '10:30 AM', href: 'kalakshetra'},
+      {name: 'Spin the Yarn', time: '12:00 PM', href: 'lit'},
+      {name: 'Telugu antakshri', time: '03:00 PM', href: 'antakshri'},
+      {name: 'Hindi antakshri', time: '03:00 PM', href: 'antakshri'},
+      {name: 'Zombie Zone Finals', time: '09:00 AM', href: 'gaming'},
+      {name: 'Treasure Hunt', time: '05:00 PM', href: 'lit'},
+      {name: 'Music and Dance night', time: '07:30 PM', href: 'lolz'}
     ]
   },
 ];
