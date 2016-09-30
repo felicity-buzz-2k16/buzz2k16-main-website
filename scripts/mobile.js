71dd0681fc2106984675d97e316839a6fc1e2f3f
Megh Parikh <meghprkh@gmail.com>
2016-10-01 04:24:15 +0530

    Add simple mobile website

---
+++ scripts/mobile.js
@@ -0,0 +1,34 @@
+var getDateDiv = function (date) {
+	return "<h2 style=\"text-align:center;\">" + date + "</h2><br>";
+}
+
+var pageroot = 'https://felicity.iiit.ac.in/buzz/'
+var getDateEvent = function (event) {
+	return `<a href="${pageroot + event.href}"><article class=\"card\">
+			<header>
+				<button>${event.name}</button>
+				<button class="dangerous">${event.time}</button>
+			</header>
+		</article>
+		</a>`
+}
+
+var getDateList = function (date, event_list) {
+		console.log(date)
+		var ret = '';
+		for (var i of event_list) {
+			ret += getDateEvent(i);
+		}
+		return  "<div>" + getDateDiv(date) + "<div>" + ret + "</div></div><br>";
+		}
+
+var eventsHTML = '';
+
+for (var i of scheduleJSON) {
+	console.log(i)
+		console.log(getDateList (i.date, i.events))
+		eventsHTML += getDateList (i.date, i.events);
+}
+
+var mainlist = document.getElementById('mainlist');
+mainlist.innerHTML = eventsHTML;
