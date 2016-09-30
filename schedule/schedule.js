24604484cbd187fb6a858649abb5edd4ee39bf98
Megh Parikh <meghprkh@gmail.com>
2016-10-01 04:37:26 +0530

    Remove schedule from non-portals

--- schedule/schedule.js
+++ schedule/schedule.js
@@ -9,10 +9,10 @@ window.scheduleJSON = [
   {
     date: '30 September',
     events: [
-      {name: 'Sports(Futsal, 21 Points, Gully Cricket)', time: '05:00 PM', href: 'sports'},
-      {name: 'Rubik\'s Cube', time: '05:30 PM', href: 'rubikscube'},
-      {name: 'Zombie Zone', time: '07:00 PM', href: 'gaming'},
-      {name: 'Na Show Na Estam', time: '08:00 PM', href: 'misc'},
+      {name: 'Sports(Futsal, 21 Points, Gully Cricket)', time: '05:00 PM'},
+      {name: 'Rubik\'s Cube', time: '05:30 PM'},
+      {name: 'Zombie Zone', time: '07:00 PM'},
+      {name: 'Na Show Na Estam', time: '08:00 PM'},
     ]
   },
   {
@@ -21,32 +21,32 @@ window.scheduleJSON = [
       {name: 'Hack In', time: '12:00 AM', href: 'hackin'},
       {name: 'Gordian Knot', time: '10:00 AM', href: 'gordianknot'},
       {name: 'Lit Quiz', time: '02:00 PM', href: 'lit'},
-      {name: 'Poster Making', time: '03:30 PM', href: 'kalakshetra'},
-      {name: 'Pokemon Showdown!', time: '03:30 PM', href: 'gaming'},
-      {name: 'Sports(Footsal, 21 Points, Gully Cricket)', time: '05:00 PM', href: 'sports'},
-      {name: 'Rangoli Making', time: '05:00 PM', href: 'kalakshetra'},
-      {name: 'JAM/ Air Crash', time: '06:00 PM', href: 'jam'},
-      {name: 'Design It', time: '06:00 PM', href: 'designit'},
-      {name: 'Zombie Zone', time: '07:00 PM', href: 'gaming'},
-      {name: 'Hackathon', time: '07:00', href: 'hackathon'},
-      {name: 'Mini Militia', time: '09:00 PM', href: 'minimilitia'},
-      {name: 'CodeCraft', time: '10:00 PM', href: 'codecraft'},
+      {name: 'Poster Making', time: '03:30 PM'},
+      {name: 'Pokemon Showdown!', time: '03:30 PM'},
+      {name: 'Sports(Footsal, 21 Points, Gully Cricket)', time: '05:00 PM'},
+      {name: 'Rangoli Making', time: '05:00 PM'},
+      {name: 'JAM/ Air Crash', time: '06:00 PM'},
+      {name: 'Design It', time: '06:00 PM'},
+      {name: 'Zombie Zone', time: '07:00 PM'},
+      {name: 'Hackathon', time: '07:00'},
+      {name: 'Mini Militia', time: '09:00 PM'},
+      {name: 'CodeCraft', time: '10:00 PM'},
     ]
   },
   {
     date: '2 October',
     events: [
-      {name: 'Hackathon', time: '00:00', href: 'hackathon'},
-      {name: 'Sports(Footsal, 21 Points, Gully Cricket)', time: '08:00 AM', href: 'sports'},
-      {name: 'Zombie Zone Finals', time: '09:00 AM', href: 'gaming'},
-      {name: 'Arduino', time: '09:00 AM - 12:00 Noon', href: 'arduino'},
-      {name: 'Friends + Anime Quiz - Miscellaneous', time: '09:30 AM - 11:30 AM', href: 'faquiz'},
-      {name: 'Face Painting', time: '10:30 AM', href: 'kalakshetra'},
-      {name: 'Spin the Yarn', time: '12:00 PM', href: 'lit'},
-      {name: 'Telugu antakshri', time: '03:00 PM', href: 'antakshri'},
-      {name: 'Hindi antakshri', time: '03:00 PM', href: 'antakshri'},
-      {name: 'Treasure Hunt', time: '05:00 PM', href: 'lit'},
-      {name: 'Music and Dance night', time: '07:30 PM', href: 'lolz'}
+      {name: 'Hackathon', time: '00:00'},
+      {name: 'Sports(Footsal, 21 Points, Gully Cricket)', time: '08:00 AM'},
+      {name: 'Zombie Zone Finals', time: '09:00 AM'},
+      {name: 'Arduino', time: '09:00 AM - 12:00 Noon'},
+      {name: 'Friends + Anime Quiz - Miscellaneous', time: '09:30 AM - 11:30 AM'},
+      {name: 'Face Painting', time: '10:30 AM'},
+      {name: 'Spin the Yarn', time: '12:00 PM'},
+      {name: 'Telugu antakshri', time: '03:00 PM'},
+      {name: 'Hindi antakshri', time: '03:00 PM'},
+      {name: 'Treasure Hunt', time: '05:00 PM'},
+      {name: 'Music and Dance night', time: '07:30 PM'}
     ]
   },
 ];
