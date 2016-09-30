926f5289004b3d1a2c6fbd618a298b6f3e0f71f0
Sourabh Rajguru <srsoccerfan@gmail.com>
2016-09-30 19:41:56 +0530

    Add buzz days and today on the calendar

--- schedule/script.js
+++ schedule/script.js
@@ -59,7 +59,6 @@ var getDateEvent = function (event) {
 }
 
 var getDateList = function (date, event_list) {
-  console.log(date)
   var ret = '';
   for (var i of event_list) {
     ret += getDateEvent(i);
@@ -70,10 +69,25 @@ var getDateList = function (date, event_list) {
 var eventsHTML = '';
 
 for (var i of scheduleJSON) {
-  console.log(i)
-  console.log(getDateList (i.date, i.events))
   eventsHTML += getDateList (i.date, i.events);
 }
 
 var mainlist = document.getElementById('mainlist');
 mainlist.innerHTML = eventsHTML;
+
+// Mark today on the calendar
+var today = new Date();
+var day = today.getDate();
+var month = today.getMonth() + 1;
+
+var tds = document.querySelectorAll('#oct tbody td');
+
+if (month === 9) {
+  tds = document.querySelectorAll('#sept tbody td');
+}
+
+for (var i = 0; i < tds.length; i++) {
+  if (parseInt (tds[i].innerText) === day) {
+    tds[i].className = "today";
+  }
+}
